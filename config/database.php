9bfbcb4bec7a3fa222277b52aea623e63143c515
Kittipoomnamuangrak <6514120010@mut.ac.th>
2023-08-24 20:22:51 +0700

    first commit

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'blog010'),
+            'database' => env('DB_DATABASE', 'blog009'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
